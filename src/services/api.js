674fcff05d34a7ff2a67bd9d7a70ab716aa4d777
Akanksha Agrawal <kumarianshuagrawal847@gmail.com>
2024-12-05 22:57:40 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  //baseURL: "http://localhost:5000", // Base URL for the backend
-  baseURL: "https://obe-backend-m45d-m1rrd745t-akankshas-projects-ed18bffa.vercel.app/",
+ 
+  baseURL: "https://obe-backend-3.onrender.com",
 });
 
 export default api;
