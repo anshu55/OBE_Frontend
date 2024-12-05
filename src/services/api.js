3387686f148a00454b76ab5be7e04acc96d5d492
Akanksha Agrawal <kumarianshuagrawal847@gmail.com>
2024-12-05 21:34:32 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000", // Base URL for the backend
-  //baseURL: "http://obe-backend-one.vercel.app",
+  //baseURL: "http://localhost:5000", // Base URL for the backend
+  baseURL: "https://obe-backend-m45d-m1rrd745t-akankshas-projects-ed18bffa.vercel.app/",
 });
 
 export default api;
